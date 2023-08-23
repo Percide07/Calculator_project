50989c062ec646e8bca31b8d380a04f244dccde7
Percide07 <percidehonhonou@gmail.com>
2023-08-24 00:30:50 +0100

    Adding js modification

---
+++ script.js
@@ -0,0 +1,89 @@
+// screen
+const screen= document.querySelector('#screen');
+// Operator
+const plus  = document.querySelector('#plus');
+const minus = document.querySelector('#min');
+const div   = document.querySelector('#div');
+const times = document.querySelector('#time')
+// Others buttons
+const mod   = document.querySelector('#mod');
+const dec   = document.querySelector('#dec');
+const ac    = document.querySelector('#ac');
+const plsm  = document.querySelector('#plsm');
+// Number
+const one   = document.querySelector('#one');
+console.log(one)
+one.addEventListener('click', function(event){
+    let resultOne = event.target.value;
+    
+});
+const two   = document.querySelector('#two');
+console.log(two)
+two.addEventListener('click', function(event){
+    let resultTwo = event.target.value;
+
+});
+const three = document.querySelector('#three');
+console.log(three)
+three.addEventListener('click', function(event){
+    let resultThree = event.target.value; 
+
+});
+const four = document.querySelector('#four');
+console.log(four)
+four.addEventListener('click', function(event){
+    let resultFour = event.target.value
+    // console.log(event.target.value);
+    // return append(four);
+
+});
+const five = document.querySelector('#five');
+console.log(five)
+five.addEventListener('click', function(event){
+    let resultFive = event.target.value
+    // console.log(event.target.value);
+    // return append(five);
+
+});
+const six = document.querySelector('#six');
+console.log(six)
+six.addEventListener('click', function(event){
+    let resultSix = event.target.value
+    // console.log(event.target.value);
+    // return append(six);
+
+});
+const seven = document.querySelector('#sev');
+console.log(seven)
+seven.addEventListener('click', function(event){
+    let resultSeven = event.target.value
+    // console.log(event.target.value);
+    // return append(seven);
+
+});
+const eight = document.querySelector('#eight');
+console.log(eight)
+eight.addEventListener('click', function(event){
+    let resultEight = event.target.value
+    // console.log(event.target.value);
+    // return append(eight);
+
+});
+const nine = document.querySelector('#nine');
+console.log(nine)
+nine.addEventListener('click', function(event){
+    let resultNine = event.target.value
+    // console.log(event.target.value);
+    // return append(nine);
+
+});
+const zero = document.querySelector('#zero');
+console.log(zero)
+zero.addEventListener('click', function(event){
+    let resultZero = event;
+    console.log(resultZero);
+    // console.log(screen.append(resultZero));
+    // return screen.append(resultZero);
+    // return append(zero);
+
+});
